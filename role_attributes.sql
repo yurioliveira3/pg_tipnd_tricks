bc6fd1bdd2ad9c9d7a491fdf2cbe994b9ec81a75
Yuri <47905419+yurioliveira3@users.noreply.github.com>
2023-05-29 08:55:47 -0300

    Create role_attributes.sql

---
+++ role_attributes.sql
@@ -0,0 +1,16 @@
+SELECT
+	usename AS role_name,
+	(CASE
+		WHEN usesuper AND usecreatedb THEN
+    		CAST('superuser, create database' AS pg_catalog.text)
+		WHEN usesuper THEN
+    		CAST('superuser' AS pg_catalog.text)
+		WHEN usecreatedb THEN
+    		CAST('create database' AS pg_catalog.text)
+		ELSE
+    		CAST('' AS pg_catalog.text)
+	END) AS role_attributes
+FROM
+	pg_catalog.pg_user
+ORDER BY
+	role_name DESC;
